b48e3341e43f3e3f04c47339162ef516bb37fb25
Marko - Laptop <madevcic@gmail.com>
2015-08-15 12:28:15 +0100

    initial

---
+++ LargeList/LargeList.cs
@@ -0,0 +1,525 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarkoDevcic
+{
+    /// <summary>
+    /// IList implementations that avoids Large Object Heap allocations
+    /// Currently objects larger than 85.000 bytes are allocated on the LOH, which is never compacted during GC. 
+    /// This can cause memory fragmentation and holes in the heap. 
+    /// OutOfMemory Exceptions can be thrown even if requested allocation is available in the address space.
+    /// Lists are backed by arrays and any list larger than 10.000 items is gonna have it's backing array allocated on the LOH.
+    /// Class is implemented in a way that it holds its items in List of arrays, of which each one has max size of 2^13 items, so each array sits in regular, GC compacted Heap.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    [DebuggerDisplay("Count = {Count}")]
+    [Serializable]
+    public class LargeList<T> : IList<T>
+    {
+        private const Int32 LARGE_HEAP_LIMIT = 85000;
+        private const Int32 DEFAULT_ARRAY_SIZE = 16;
+
+        private readonly Int32 arrayMaxSize;
+
+        private Int32 size = 0;
+        private Int32 version = 0;
+
+        private readonly List<T[]> arrayHolder;
+        private readonly List<Int32> arraySizes;
+
+
+        public LargeList()
+        {
+            if (typeof(T).IsValueType)
+            {
+                var typeSize = System.Runtime.InteropServices.Marshal.SizeOf(typeof(T));
+                var maxSize = LARGE_HEAP_LIMIT / typeSize;
+                arrayMaxSize = FirstSmallerPowerOf2(maxSize);
+            }
+            else
+            {
+                var maxSize = LARGE_HEAP_LIMIT / IntPtr.Size;
+                arrayMaxSize = FirstSmallerPowerOf2(maxSize);
+            }
+
+            arrayHolder = new List<T[]>();
+            arraySizes = new List<Int32>();
+        }
+
+        public LargeList(Int32 size)
+            : this()
+        {
+            if (size < 0)
+                throw new ArgumentOutOfRangeException("Size must be 0 or larger");
+
+            CreateInternalArrays(size);
+        }
+
+        private void CreateInternalArrays(Int32 size)
+        {
+            while (size >= arrayMaxSize)
+            {
+                arrayHolder.Add(new T[arrayMaxSize]);
+                arraySizes.Add(0);
+                size -= arrayMaxSize;
+            }
+            if (size > 0)
+            {
+                var lastSize = FirstLargerPowerOf2(size);
+                lastSize = lastSize < DEFAULT_ARRAY_SIZE ? DEFAULT_ARRAY_SIZE : lastSize;
+                arrayHolder.Add(new T[lastSize]);
+                arraySizes.Add(0);
+            }
+        }
+
+        public LargeList(IEnumerable<T> source)
+            : this()
+        {
+            if (source == null)
+                throw new ArgumentNullException("source");
+
+            CreateInternalArrays(source.Count());
+
+            foreach (var item in source)
+            {
+                Add(item);
+            }
+        }
+
+        private Int32 FirstSmallerPowerOf2(Int32 num)
+        {
+            if (num <= 0)
+                return 0;
+
+            var count = 0;
+            while (num > 1)
+            {
+                count++;
+                num >>= 1;
+            }
+            return 1 << count;
+        }
+
+        private Int32 FirstLargerPowerOf2(Int32 num)
+        {
+            if (num < 0)
+                return 0;
+
+            if (num == 0)
+                return 2;
+
+            var count = 0;
+            while (num > 0)
+            {
+                count++;
+                num >>= 1;
+            }
+            return 1 << count;
+        }
+
+        public int IndexOf(T item)
+        {
+            for (int i = 0; i < arrayHolder.Count; i++)
+            {
+                var index = Array.IndexOf(arrayHolder[i], item, 0, arraySizes[i]);
+                if (index >= 0)
+                    return index + (i * arrayMaxSize);
+            }
+            return -1;
+        }
+
+        public void Insert(int index, T item)
+        {
+            if (index > size)
+                throw new ArgumentOutOfRangeException();
+
+            var arrayIndex = GetArrayIndex(index);
+            var array = GetDestinationArray(ref arrayIndex);
+            var position = index - arrayIndex * arrayMaxSize;
+
+            if (position < arraySizes[arrayIndex])
+            {
+                Array.Copy(array, position, array, position + 1, arraySizes[arrayIndex] - position);
+            }
+
+            array[position] = item;
+            arraySizes[arrayIndex]++;
+            size++;
+            version++;
+        }
+
+        public void RemoveAt(int index)
+        {
+            if (index >= size)
+                throw new ArgumentOutOfRangeException();
+
+            var arrayIndex = GetArrayIndex(index);
+            var array = arrayHolder[arrayIndex];
+            var position = index - arrayIndex * arrayMaxSize;
+            size--;
+
+            if (position < arraySizes[arrayIndex])
+            {
+                Array.Copy(array, position + 1, array, position, arraySizes[arrayIndex] - 1 - position);
+            }
+
+            if (arrayIndex == arrayHolder.Count - 1)
+            {
+                array[arraySizes[arrayIndex]] = default(T);
+                arraySizes[arrayIndex]--;
+            }
+            else
+            {
+                var indexCount = arrayIndex;
+
+                while (indexCount < arrayHolder.Count - 1)
+                {
+                    var shiftArray = arrayHolder[indexCount];
+                    var nextArrayFirstItem = arrayHolder[indexCount + 1][0];
+                    if (indexCount != arrayIndex)
+                        Array.Copy(shiftArray, 1, shiftArray, 0, (arraySizes[indexCount] - 1));
+                    array[arraySizes[indexCount] - 1] = nextArrayFirstItem;
+                    indexCount++;
+                }
+                var lastArrayIndex = arrayHolder.Count - 1;
+                var lastArray = arrayHolder[lastArrayIndex];
+                Array.Copy(lastArray, 1, lastArray, 0, (arraySizes[lastArrayIndex] - 1));
+            }
+            version++;
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                if (index >= size)
+                    throw new ArgumentOutOfRangeException();
+
+                var arrayIndex = GetArrayIndex(index);
+                var position = index - arrayIndex * arrayMaxSize;
+                var array = arrayHolder[arrayIndex];
+
+                if (position >= array.Length)
+                    throw new ArgumentOutOfRangeException();
+
+                return array[position];
+            }
+            set
+            {
+                if (index >= size)
+                    throw new ArgumentOutOfRangeException();
+
+                var arrayIndex = GetArrayIndex(index);
+                var position = index - arrayIndex * arrayMaxSize;
+                var array = arrayHolder[arrayIndex];
+
+                if (position >= array.Length)
+                    throw new ArgumentOutOfRangeException();
+
+                array[position] = value;
+                version++;
+            }
+        }
+
+        public void Add(T item)
+        {
+            var index = GetArrayIndex(size);
+            var array = GetDestinationArray(ref index);
+            var position = arraySizes[index];
+            array[position] = item;
+            arraySizes[index]++;
+            size++;
+            version++;
+        }
+
+        private Int32 GetArrayIndex(Int32 index)
+        {
+            var ratio = (double)index / (double)arrayMaxSize;
+            return (Int32)Math.Floor(ratio);
+        }
+
+
+        private T[] GetDestinationArray(ref Int32 arrayIndex)
+        {
+            T[] array;
+            if (arrayHolder.Count == arrayIndex)
+            {
+                array = new T[DEFAULT_ARRAY_SIZE];
+                arrayHolder.Insert(arrayIndex, array);
+                arraySizes.Insert(arrayIndex, 0);
+                return array;
+            }
+            else
+            {
+                array = arrayHolder[arrayIndex];
+                if (array.Length == arraySizes[arrayIndex])
+                {
+                    var resizeIndex = Resize(arrayIndex);
+                    if (arrayIndex == resizeIndex)
+                    {
+                        array = arrayHolder[arrayIndex];
+                    }
+                    else
+                    {
+                        arrayIndex = resizeIndex;
+                        return GetDestinationArray(ref arrayIndex);
+                    }
+                }
+                return array;
+            }
+        }
+
+        private Int32 Resize(Int32 arrayindex)
+        {
+            var oldSize = arraySizes[arrayindex];
+            if (arrayMaxSize >> 1 >= oldSize)
+            {
+                var newSize = oldSize << 1;
+                var oldArray = arrayHolder[arrayindex];
+                var newArray = new T[newSize];
+                Array.Copy(oldArray, 0, newArray, 0, oldArray.Length);
+                arrayHolder[arrayindex] = newArray;
+
+                return arrayindex;
+            }
+            else
+            {
+                return ++arrayindex;
+            }
+        }
+
+        public void Clear()
+        {
+            arrayHolder.Clear();
+            arraySizes.Clear();
+            size = 0;
+            version++;
+        }
+
+        public bool Contains(T item)
+        {
+            var comparer = EqualityComparer<T>.Default;
+            for (int i = 0; i < arrayHolder.Count; i++)
+            {
+                var array = arrayHolder[i];
+                for (int j = 0; j < arraySizes[i]; j++)
+                {
+                    if (comparer.Equals(item, array[j]))
+                        return true;
+                }
+            }
+
+            return false;
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            if (array == null)
+                throw new ArgumentNullException("array");
+
+            if (arrayIndex < 0 || arrayIndex > array.Length)
+                throw new IndexOutOfRangeException();
+
+            if (array.Length - arrayIndex < size)
+                throw new IndexOutOfRangeException();
+
+            for (int i = 0; i < arrayHolder.Count; i++)
+            {
+                Array.Copy(arrayHolder[i], 0, array, i * arraySizes[i], arraySizes[i]);
+            }
+        }
+
+        public void ForEach(Action<T> action)
+        {
+            for (int i = 0; i < arrayHolder.Count; i++)
+            {
+                var array = arrayHolder[i];
+                for (int j = 0; j < arraySizes[i]; j++)
+                {
+                    action(array[j]);
+                }
+            }
+        }
+
+        public int Count
+        {
+            get { return size; }
+        }
+
+        public bool IsReadOnly
+        {
+            get { return false; }
+        }
+
+        public bool Remove(T item)
+        {
+            var position = IndexOf(item);
+            if (position < 0)
+                return false;
+
+            RemoveAt(position);
+
+            return true;
+        }
+
+        public void Reverse()
+        {
+            foreach (var array in arrayHolder)
+            {
+                Array.Reverse(array);
+            }
+            arrayHolder.Reverse();
+
+            version++;
+        }
+
+        public void Sort()
+        {
+            var comparer = Comparer<T>.Default;
+            HeapSort(comparer);
+        }
+
+        public void Sort(IComparer<T> comparer)
+        {
+            if (comparer == null)
+                throw new ArgumentNullException("comparer");
+
+            HeapSort(comparer);
+        }
+
+        private void HeapSort(IComparer<T> comparer)
+        {
+            var middle = size / 2;
+            var heapSize = size;
+            for (int i = middle; i >= 0; i--)
+            {
+                BuildMaxHeap(i, comparer, heapSize);
+            }
+
+            for (int i = size - 1; i > 0; i--)
+            {
+                Swap(i, 0);
+                heapSize--;
+                BuildMaxHeap(0, comparer, heapSize);
+            }
+
+            version++;
+        }
+
+        private void BuildMaxHeap(Int32 index, IComparer<T> comparer, Int32 heapSize)
+        {
+            while (index * 2 + 1 < heapSize)
+            {
+                int next;
+
+                var left = index * 2 + 1;
+                if (comparer.Compare(this[left], this[index]) > 0)
+                {
+                    next = left;
+                }
+                else
+                {
+                    next = index;
+                }
+
+                var right = index * 2 + 2;
+                if (right < heapSize && comparer.Compare(this[right], this[next]) > 0)
+                {
+                    next = right;
+                }
+
+                if (next != index)
+                {
+                    Swap(next, index);
+                    index = next;
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+
+        private void Swap(Int32 left, Int32 right)
+        {
+            var temp = this[left];
+            this[left] = this[right];
+            this[right] = temp;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return new Enumerator(this);
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return new Enumerator(this);
+        }
+
+        [Serializable]
+        internal struct Enumerator : IEnumerator<T>
+        {
+            private readonly LargeList<T> list;
+            private readonly Int32 listVersion;
+            private Int32 index;
+            private T current;
+
+            internal Enumerator(LargeList<T> _list)
+            {
+                list = _list;
+                listVersion = _list.version;
+                current = default(T);
+                index = 0;
+            }
+
+            public T Current
+            {
+                get { return current; }
+            }
+
+            public void Dispose()
+            {
+
+            }
+
+            object System.Collections.IEnumerator.Current
+            {
+                get { return current; }
+            }
+
+            public bool MoveNext()
+            {
+                CheckVersion();
+
+                if (index < list.size)
+                {
+                    current = list[index];
+                    index++;
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            private void CheckVersion()
+            {
+                if (listVersion != list.version)
+                    throw new InvalidOperationException("Collection was modified");
+            }
+
+            public void Reset()
+            {
+                index = 0;
+                current = default(T);
+            }
+        }
+
+    }
+}
---
+++ LargeListTests/LargeListTests.cs
@@ -0,0 +1,298 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using MarkoDevcic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MarkoDevcic.Tests
+{
+    [TestClass()]
+    public class LargeListTests
+    {
+        [TestMethod]
+        public void TestAdd()
+        {
+            var size = 1 << 20;
+            var list = CreateList(size);
+
+            for (int i = 0; i < size; i++)
+            {
+                Assert.AreEqual(i, list[i]);
+            }
+        }
+
+
+        [TestMethod]
+        public void TestContains()
+        {
+            var size = 1 << 14;
+            var list = CreateList(size);
+
+            for (int i = 0; i < size; i++)
+            {
+                Assert.IsTrue(list.Contains(i));
+            }
+
+            Assert.IsFalse(list.Contains(Int32.MaxValue));
+        }
+
+        [TestMethod]
+        public void TestCount()
+        {
+            var size = 1 << 20;
+            var list = CreateList(size);
+
+            Assert.AreEqual(list.Count, size);
+        }
+
+        [TestMethod]
+        public void TestEnumerator()
+        {
+            var list = new LargeList<Int32>();
+            var size = 1 << 20;
+            var helperArray = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                list.Add(i);
+                helperArray[i] = i;
+            }
+
+            foreach (var num in list)
+            {
+                Assert.AreEqual(num, helperArray[num]);
+            }
+        }
+
+        [TestMethod]
+        public void TestClear()
+        {
+            var size = 1 << 20;
+            var list = CreateList(size);
+
+            list.Clear();
+            Assert.AreEqual(list.Count, 0);
+            foreach (var item in list)
+            {
+                Assert.Fail("Shouldn't get here");
+            }
+
+            for (int i = 0; i < size; i++)
+            {
+                list.Add(i);
+            }
+
+            for (int i = 0; i < size; i++)
+            {
+                Assert.AreEqual(i, list[i]);
+            }
+        }
+
+        [TestMethod]
+        public void TestCopyTo()
+        {
+            var size = 1 << 20;
+            var list = CreateList(size);
+
+            var array = new int[size];
+            list.CopyTo(array, 0);
+
+            Assert.AreEqual(array.Length, list.Count);
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Assert.AreEqual(array[i], list[i]);
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void TestCollectionWasModified()
+        {
+            var size = 1 << 20;
+            var list = CreateList(size);
+
+            Task.Run(delegate { Thread.Sleep(1); list.Add(1); });
+
+            foreach (var item in list)
+            {
+
+            }
+        }
+
+        [TestMethod]
+        public void TestSizeConstructor()
+        {
+            var list = new LargeList<Int32>(1);
+            list.Add(1);
+            Assert.AreEqual(list.Count, 1);
+            foreach (var item in list)
+            {
+                Assert.AreEqual(item, 1);
+            }
+
+            var size = (1 << 14) + 1;
+            list = new LargeList<Int32>(size);
+
+            for (int i = 0; i < size * 2; i++)
+            {
+                list.Add(i);
+            }
+
+            for (int i = 0; i < size * 2; i++)
+            {
+                Assert.AreEqual(list[i], i);
+            }
+        }
+
+        [TestMethod]
+        public void TestEnumerableConsuctor()
+        {
+            var size = (1 << 14) + 2500;
+            var array = new Int32[size];
+            for (int i = 0; i < size; i++)
+            {
+                array[i] = i;
+            }
+
+            var list = new LargeList<Int32>(array);
+
+            for (int i = 0; i < size; i++)
+            {
+                Assert.AreEqual(array[i], list[i]);
+            }
+        }
+
+        [TestMethod]
+        public void TestIndexOf()
+        {
+            var size = (1 << 14) + 2113;
+            var list = CreateList(size);
+
+            for (int i = 0; i < size; i++)
+            {
+                Assert.AreEqual(i, list.IndexOf(i));
+            }
+
+            Assert.AreEqual(-1, list.IndexOf(-1));
+        }
+
+        [TestMethod]
+        public void TestInsert()
+        {
+            var list = new LargeList<Int32>();
+            list.Insert(0, 0);
+            list.Add(2);
+
+            list.Insert(1, 1);
+
+            for (int i = 0; i < 3; i++)
+            {
+                Assert.AreEqual(i, list[i]);
+            }
+        }
+
+        [TestMethod]
+        public void TestInsertInsideSecondArray()
+        {
+            var size = (1 << 14) + 1;
+            var list = CreateList(size);
+            list.Insert(size, size);
+            list.Insert(size - 1, size - 1);
+
+            Assert.AreEqual(list.Count, size + 2);
+
+            for (int i = size + 1; i >= size; i--)
+            {
+                Assert.AreEqual(i - 1, list[i]);
+            }
+        }
+
+        [TestMethod]
+        public void TestRemoveAt()
+        {
+            var size = (1 << 14) + 1234;
+            var list = CreateList(size);
+            list.RemoveAt(size - 1);
+
+            Assert.AreEqual(list.Count, size - 1);
+
+            list.RemoveAt(0);
+
+            Assert.AreEqual(list.Count, size - 2);
+
+            for (int i = 0; i < size - 2; i++)
+            {
+                Assert.AreEqual(i + 1, list[i]);
+            }
+        }
+
+        [TestMethod]
+        public void TestReverse()
+        {
+            var size = 1 << 20;
+            var list = CreateList(size);
+
+            for (int i = 0; i < size; i++)
+            {
+                list.Add(i);
+            }
+
+            list.Reverse();
+
+            for (int i = size - 1; i >= 0; i--)
+            {
+                Assert.AreEqual(list[size - 1 - i], i);
+            }
+        }
+
+        [TestMethod]
+        public void TestSort()
+        {
+            var size = (1 << 16) + 1234;
+            var list = new LargeList<Int32>();
+            var helperArray = new Int32[size];
+            var random = new Random();
+
+            for (int i = 0; i < size; i++)
+            {
+                var n = random.Next();
+                helperArray[i] = n;
+                list.Add(n);
+            }
+
+            Array.Sort(helperArray);
+            list.Sort();
+
+            for (int i = 0; i < size; i++)
+            {
+                Assert.AreEqual(helperArray[i], list[i]);
+            }
+        }
+
+        [TestMethod]
+        public void TestRemove()
+        {
+            var size = (1 << 14) + 2134;
+            var list = CreateList(size);
+
+            Assert.IsTrue(list.Remove(0));
+            Assert.IsTrue(list.Remove(size - 1));
+            Assert.IsFalse(list.Remove(Int32.MaxValue));
+        }
+
+        private LargeList<Int32> CreateList(int size)
+        {
+            var list = new LargeList<Int32>();
+            for (int i = 0; i < size; i++)
+            {
+                list.Add(i);
+            }
+
+            return list;
+        }
+    }
+}
